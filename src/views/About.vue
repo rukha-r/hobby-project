d25a62ae733db3ae9a45891bbd8a034224e57b7c
rukha.r <ruzievrukha@gmail.com>
2020-12-30 16:30:44 +0200

    Little upgrade in About page...

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
     <div class="about-me">
      <div class="inner-card">
       <img :src="author_image" alt="_authors picture">
-      <p>Hey there.My name is <span class="name">{{ name }}</span> and I am a self-thought front-end developer.Here you can read about me and check and rate my work.There is some of my in <router-link :title="projectsTitle" to="/Projects"> projects </router-link> page !</p>
+      <p>Hey there.My name is <span class="name">{{ name }}</span> and I am a self-thought front-end developer.Here you can read about me and check and rate my work.There is some of my little projects in "<router-link :title="projectsTitle" to="/Projects"> projects </router-link>" page !</p>
       <p> I strated coding back to 2019 and found it very interesting.I sterted lerning <a :href="html.url" :title="html.title" target="_blank">{{html.name}}</a>.After getting confortable with it, I was strogling learning <a :href="css.url" :title="css.title" target="_blank">{{css.name}}</a>.It was so hard and I was almost destroying my laptop,coz I was so upset that I couldnt keep up.Well it happend until I strted learning <a :href="js.url" :title="js.name" target="_blank">{{js.name}}</a>so things got harder.But I didn't quite and today I am <a :href="vue.url" :title="vue.name" target="_blank">{{vue.name}}</a> developer...</p>
     </div>
     </div>
